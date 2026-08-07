86a8f29dc7ec16b3687c99d3d71a08d5a5a6ba30
MUHANNED PV <p.vmuhannad786@gmail.com>
2026-08-08 00:45:56 +0530

    Update variables.tf

--- DevOps-Project-01/infrastructure/modules/rds/variables.tf
+++ DevOps-Project-01/infrastructure/modules/rds/variables.tf
@@ -1,5 +1,3 @@
-# RDS Module
-
 variable "environment" {
   description = "Environment name"
   type        = string
@@ -35,19 +33,4 @@ variable "db_password" {
   description = "Database password"
   type        = string
   sensitive   = true
-}
-
-output "rds_instance_id" {
-  description = "RDS instance ID"
-  value       = aws_db_instance.main.id
-}
-
-output "rds_instance_endpoint" {
-  description = "RDS instance endpoint"
-  value       = aws_db_instance.main.endpoint
-}
-
-output "rds_instance_port" {
-  description = "RDS instance port"
-  value       = aws_db_instance.main.port
 }
