26c144aa84d4883dd3caee5c148f0faf2b3a1b4e
MUHANNED PV <p.vmuhannad786@gmail.com>
2026-08-08 00:04:26 +0530

    Change AWS region and availability zones to ap-south-1

--- DevOps-Project-01/infrastructure/variables.tf
+++ DevOps-Project-01/infrastructure/variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   description = "AWS region to deploy resources"
   type        = string
-  default     = "us-east-1"
+  default     = "ap-south-1"
 }
 
 variable "environment" {
@@ -31,7 +31,7 @@ variable "private_subnets" {
 variable "availability_zones" {
   description = "Availability zones"
   type        = list(string)
-  default     = ["us-east-1a", "us-east-1b"]
+  default     = ["ap-south-1a", "ap-south-1b"]
 }
 
 variable "db_name" {
