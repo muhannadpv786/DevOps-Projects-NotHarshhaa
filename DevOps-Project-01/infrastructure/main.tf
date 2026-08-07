7063c310169fb5068e9a9a39f93e48a80c245b3c
MUHANNED PV <p.vmuhannad786@gmail.com>
2026-08-08 00:07:35 +0530

    Change S3 bucket and region for Terraform backend
    Updated S3 backend configuration for Terraform.

--- DevOps-Project-01/infrastructure/main.tf
+++ DevOps-Project-01/infrastructure/main.tf
@@ -11,9 +11,9 @@ terraform {
   
   backend "s3" {
     # Update these values according to your setup
-    # bucket = "your-terraform-state-bucket"
+    # bucket = "muhannad-terraform-backend-bucket"
     # key    = "java-app/terraform.tfstate"
-    # region = "us-east-1"
+    # region = "ap-south-1"
   }
 }
 
